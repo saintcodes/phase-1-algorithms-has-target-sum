edfae402b0dd971004be3da9d516373937762181
Michelle <clairsin@gmail.com>
2022-02-23 17:58:13 -0500

    commit tree 1b367d820bacdc63ab03d8eb5c5178afe77fe408

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
+
+// //alternative solution
+// function hasTargetSum(array, target) {
+//   //creating an object to push seen numbers to
+//   const seenNumbers = {};
+
+//   //iterating through each number of array
+//   for (const number of array) {
+//   //matching to see if the complement number is equal to target minus current array number
+//     const complement = target - number;
+//   //checking if any key in object is our complement, if so return true
+//     if (complement in seenNumbers) return true;
+//   // otherwise, add that number to seenNumbers object
+//     seenNumbers[number] = true;
+//   }
+//   return false;
+// }
+
+
+  ///below code does not work
+  // const randomNum1 = array[Math.floor(Math.random() * array.length)]
+  // const randomNum2 = array[Math.floor(Math.random() * array.length)]
+  // if (randomNum1 + randomNum2 === target) {
+  //   return true
+  // } else {
+  //   return false
+  // }
 }
 
 /* 
   Write the Big O time complexity of your function here
-*/
+    Runtime: O(n^2)
+    quadratic as we have loops within loops. as array increases, runtime will increase
+    Space: 0(n)
+
+  */
 
 /* 
   Add your pseudocode here
+  take the sum of one random number + another random number (but not the same as first number)
+  if total sum should equal to target, return true
+  else return false
+  
 */
 
 /*
